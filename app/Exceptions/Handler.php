67d03b82384c0b537171588970cfc4f903804cca
wtransporter <vladapantic@hotmail.com>
2021-04-13 10:28:02 +0200

    Add custom error 404 page

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -55,6 +55,12 @@ class Handler extends ExceptionHandler
             return redirect()->back()->withErrors('WebService Error !');
         }
 
+        if ($this->isHttpException($exception)) {
+            if ($exception->getCode() == 404) {
+                return response()->view('errors.404', [], 404);
+            }
+        }
+
         return parent::render($request, $exception);
     }
 }
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="section section-breadcrumbs text-center">    
+        <h3 style="color: white;">Page not found</h3>
+    </div>
+    <div class="section">
+        <div class="container">
+            <div class="row">
+                <div class="text-center">
+                    <h1 style="font-size: 80px">404</h1>
+                    <p>
+                        <h4>Oops! Something went wrong.</h4>
+                    </p>
+                    <a class="btn btn-blue" href="/">HOME</a>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
